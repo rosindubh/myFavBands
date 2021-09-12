123a74724ef5c632722b829a9950bcc9ddedfab6
rosindubh <p.welsby2@ntlworld.com>
2021-09-12 08:33:14 +0100

    build connection and test

---
+++ src/db/connection.js
@@ -0,0 +1,16 @@
+// phil welsby - 12 sept 2021
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Successfully connected...');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connection();
+
